0e37e0ef30a41f67138d47432500b9f6e5cb78cf
Hardik Singhal <44445053+MarvelousHacker@users.noreply.github.com>
2018-12-11 22:59:11 +0530

    Update max_pairwise_product.java

--- algorithmic_toolbox/week_1/maximum_pairwise_product/max_pairwise_product.java
+++ algorithmic_toolbox/week_1/maximum_pairwise_product/max_pairwise_product.java
@@ -3,21 +3,7 @@ import java.util.*;
 import java.io.*;
 
 public class MaxPairwiseProduct {
-
-    // slower methon O(n^2)
-    static long getMaxPairwiseProduct(int[] numbers) {
-        long result = 0;
-        int n = numbers.length;
-        for (int i = 0; i < n; ++i) {
-            for (int j = i + 1; j < n; ++j) {
-                if ((long)numbers[i] * numbers[j] > result) {
-                    result = (long)numbers[i] * numbers[j];
-                }
-            }
-        }
-        return result;
-    }
-
+    
     // faster method
     static long getMaxPairwiseProductFast(int[] numbers) {
         int nSize = numbers.length;
@@ -50,37 +36,6 @@ public class MaxPairwiseProduct {
         System.out.println(getMaxPairwiseProductFast(numbers));
         
 
-    /*
-    // stress test
-    while (true) {
-
-        Random ran = new Random();
-        int max = 5;
-        int min = 2;
-        int randomNum = ran.nextInt((max - min) + 1) + min;
-
-        System.out.println("Random number: " + randomNum);
-
-        int[] list = new int[randomNum];
-
-        for (int i = 0; i < randomNum; i++) {
-            list[i] = ran.nextInt((10) + 1);
-            System.out.println(list[i]);
-        }
-        System.out.println("");
-
-        double result1 = getMaxPairwiseProduct(list);
-        double result2 = getMaxPairwiseProductFast(list);
-
-        if (result1 != result2) {
-            System.out.println("Error! slow:  " + result1 + " fast: " + result2);
-            break;
-        } else {
-            System.out.println("OK");
-        }
-    }
-     */
-}
     // scanning code
     static class FastScanner {
         BufferedReader br;
